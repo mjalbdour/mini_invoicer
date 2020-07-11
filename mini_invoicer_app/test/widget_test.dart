607c70c39f249738c46c37ca69b4c20d51e74748
mjalbdour <mjalbdour@gmail.com>
2020-07-11 15:24:42 +0300

    fixed testing error

--- mini_invoicer_app/test/widget_test.dart
+++ mini_invoicer_app/test/widget_test.dart
@@ -13,7 +13,7 @@ import 'package:mini_invoicer_app/main.dart';
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
     // Build our app and trigger a frame.
-    await tester.pumpWidget(MyApp());
+    await tester.pumpWidget(MiniInvoicer());
 
     // Verify that our counter starts at 0.
     expect(find.text('0'), findsOneWidget);
