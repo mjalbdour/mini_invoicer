261a9e2e35c54cb179abc1ef4185152e61fd696f
mjalbdour <mjalbdour@gmail.com>
2020-07-12 10:58:10 +0300

    added appbar with home title

--- mini_invoicer_app/lib/screens/home_screen.dart
+++ mini_invoicer_app/lib/screens/home_screen.dart
@@ -5,6 +5,9 @@ class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        title: Text("Home"),
+      ),
       body: ListView(
         padding: EdgeInsets.all(16.0),
         children: <Widget>[
