9db90505586349b8d74622b01783389997a6e28d
mj <mjalbdour@gmail.com>
2020-07-29 09:37:35 +0300

    injected customer detail screen stateless for lighter code

--- mini_invoicer_app/lib/screens/customer/customer_detail_screen.dart
+++ mini_invoicer_app/lib/screens/customer/customer_detail_screen.dart
@@ -1,26 +1,37 @@
 import 'package:flutter/material.dart';
-import 'package:mini_invoicer_app/screens/customer/customer_edit_screen.dart';
-import 'package:mini_invoicer_app/screens/customer/customer_list_screen.dart';
+import 'package:mini_invoicer_app/models/customer_model.dart';
+import 'package:mini_invoicer_app/screens/customer/customer_create_edit_screen.dart';
 
-class CustomerDetailScreen extends StatefulWidget {
-  static const String route = "${CustomerListScreen.route}/customer";
+class CustomerDetailScreen extends StatelessWidget {
+  final Customer customer;
 
-  @override
-  _CustomerDetailScreenState createState() => _CustomerDetailScreenState();
-}
-
-class _CustomerDetailScreenState extends State<CustomerDetailScreen> {
+  CustomerDetailScreen({@required this.customer});
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text("customer"),
+        title: Text("${customer.name}"),
       ),
       floatingActionButton: FloatingActionButton(
         onPressed: () {
-          Navigator.pushNamed(context, CustomerEditScreen.route);
+          Navigator.of(context).push(MaterialPageRoute(
+              builder: (context) =>
+                  CustomerCreateEditScreen(customer: customer)));
         },
         child: Icon(Icons.edit),
+      ),
+      body: ListView(
+        padding: EdgeInsets.all(16.0),
+        children: <Widget>[
+          Text(customer.name),
+          Text(customer.address["street"]),
+          Text(customer.address["buildingNumber"]),
+          Text(customer.address["area"]),
+          Text(customer.address["city"]),
+          Text(customer.address["postalCode"]),
+          Text(customer.address["country"]),
+          Text(customer.type),
+        ],
       ),
     );
   }
