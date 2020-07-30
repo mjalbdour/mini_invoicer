3651a01f547e903709261b74a38e0a9638bf3cb7
mjalbdour <mjalbdour@gmail.com>
2020-07-31 02:55:40 +0300

    order class almost done

--- mini_invoicer_app/lib/models/order_model.dart
+++ mini_invoicer_app/lib/models/order_model.dart
@@ -3,4 +3,33 @@ class Order {
   String customerId;
   DateTime date;
   double totalValue;
+
+  Order({
+    this.customerId,
+    this.date,
+    this.totalValue,
+  });
+
+  Order.withId({
+    this.id,
+    this.customerId,
+    this.date,
+    this.totalValue,
+  });
+
+  factory Order.fromMap(Map<String, dynamic> map, String id) {
+    return Order.withId(
+        id: id,
+        customerId: map["customerId"],
+        date: map["date"], // format then convert to date object?...
+        totalValue: map["totalValue"]);
+  }
+
+  static Map<String, dynamic> toMap(Order order) {
+    Map<String, dynamic> map = <String, dynamic>{};
+    map["customerId"] = order.customerId;
+    map["date"] = order.date;
+    map["totalValue"] = order.totalValue;
+    return map;
+  }
 }
