9dd8c20d4f4da16a82a7f82495cd1e7c584652e8
mjalbdour <mjalbdour@gmail.com>
2020-07-31 03:29:52 +0300

    implemented collectionGroup query for orders by cutomers

--- mini_invoicer_app/lib/screens/order/order_list_screen.dart
+++ mini_invoicer_app/lib/screens/order/order_list_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 
 class OrderListScreen extends StatefulWidget {
@@ -8,11 +9,46 @@ class OrderListScreen extends StatefulWidget {
 }
 
 class _OrderListScreenState extends State<OrderListScreen> {
+  final orders = Firestore.instance.collectionGroup("orders");
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: Text(OrderListScreen.title),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () {},
+        child: Icon(Icons.add),
+      ),
+      body: StreamBuilder(
+        stream: orders.getDocuments().then((value) => value).asStream(),
+        builder: (BuildContext context, AsyncSnapshot<QuerySnapshot> snapshot) {
+          if (snapshot.hasError)
+            return Center(
+              child: Text(snapshot.error),
+            );
+          switch (snapshot.connectionState) {
+            case ConnectionState.waiting:
+              return Center(
+                child: CircularProgressIndicator(),
+              );
+
+            default:
+              return ListView.builder(
+                itemBuilder: (BuildContext context, int index) {
+                  return Card(
+                    child: ListTile(
+                      title: Text(snapshot
+                          .data.documents[index].data["totalValue"]
+                          .toString()),
+                    ),
+                  );
+                },
+                itemCount: snapshot.data.documents.length,
+              );
+          }
+        },
       ),
     );
   }
