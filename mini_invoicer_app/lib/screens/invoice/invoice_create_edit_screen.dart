0218a1021653946996dede95023e08cc3df65424
mjalbdour <mjalbdour@gmail.com>
2020-08-01 15:21:54 +0300

    loading products inside invoice with amount control

--- mini_invoicer_app/lib/screens/invoice/invoice_create_edit_screen.dart
+++ mini_invoicer_app/lib/screens/invoice/invoice_create_edit_screen.dart
@@ -1,3 +1,4 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:flutter/material.dart';
 import 'package:mini_invoicer_app/models/customer_model.dart';
 import 'package:mini_invoicer_app/models/invoice_model.dart';
@@ -15,6 +16,8 @@ class InvoiceCreateEditScreen extends StatefulWidget {
 }
 
 class _InvoiceCreateEditScreenState extends State<InvoiceCreateEditScreen> {
+  final CollectionReference products =
+      Firestore.instance.collection("products");
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -35,8 +38,81 @@ class _InvoiceCreateEditScreenState extends State<InvoiceCreateEditScreen> {
             decoration: InputDecoration(labelText: "Date"),
             initialValue: DateTime.now().toString(),
           ),
+          StreamBuilder(
+              stream: products.snapshots(),
+              builder: (BuildContext context,
+                  AsyncSnapshot<QuerySnapshot> snapshot) {
+                if (snapshot.hasError) {
+                  return Center(
+                    child: Text(snapshot.error),
+                  );
+                }
+                switch (snapshot.connectionState) {
+                  case ConnectionState.waiting:
+                    return Center(
+                      child: CircularProgressIndicator(),
+                    );
+
+                  default:
+                    return ListView.builder(
+                        shrinkWrap: true,
+                        itemCount: snapshot.data.documents.length,
+                        itemBuilder: (BuildContext context, int index) {
+                          return ProductCounter(
+                              productName:
+                                  snapshot.data.documents[index].data["name"]);
+                        });
+                }
+              }),
         ],
       )),
+    );
+  }
+}
+
+class ProductCounter extends StatefulWidget {
+  final String productName;
+
+  ProductCounter({@required this.productName});
+
+  @override
+  _ProductCounterState createState() => _ProductCounterState();
+}
+
+class _ProductCounterState extends State<ProductCounter> {
+  int amount = 0;
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      mainAxisAlignment: MainAxisAlignment.spaceBetween,
+      children: <Widget>[
+        Text(widget.productName),
+        Row(
+          children: <Widget>[
+            FlatButton(
+              onPressed: amount > 0
+                  ? () {
+                      if (amount > 0) {
+                        setState(() {
+                          amount--;
+                        });
+                      }
+                    }
+                  : null,
+              child: Icon(Icons.remove),
+            ),
+            Text(amount.toString()),
+            FlatButton(
+              onPressed: () {
+                setState(() {
+                  amount++;
+                });
+              },
+              child: Icon(Icons.add),
+            )
+          ],
+        )
+      ],
     );
   }
 }
