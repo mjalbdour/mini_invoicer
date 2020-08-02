5ed4afa07af717a09b7112db68592fe161006489
mjalbdour <mjalbdour@gmail.com>
2020-08-02 20:17:27 +0300

    created firebaseAuthService class with custom user

--- mini_invoicer_app/lib/main.dart
+++ mini_invoicer_app/lib/main.dart
@@ -1,16 +1,21 @@
 import 'package:flutter/material.dart';
 import 'package:mini_invoicer_app/screens/landing_screen.dart';
 import 'package:mini_invoicer_app/screens/route_map.dart';
+import 'package:mini_invoicer_app/services/firebase_auth_service.dart';
+import 'package:provider/provider.dart';
 
 void main() => runApp(MiniInvoicer());
 
 class MiniInvoicer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Mini Invoicer",
-      initialRoute: LandingScreen.route,
-      routes: routeMap,
+    return Provider<FirebaseAuthService>(
+      create: (context) => FirebaseAuthService(),
+      child: MaterialApp(
+        title: "Mini Invoicer",
+        initialRoute: LandingScreen.route,
+        routes: routeMap,
+      ),
     );
   }
 }
