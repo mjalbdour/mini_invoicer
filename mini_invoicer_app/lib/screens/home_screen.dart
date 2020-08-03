d1b7640885bb6b9a7b90c78f2aa795ffeacafce8
mjalbdour <mjalbdour@gmail.com>
2020-08-03 13:08:45 +0300

    providing FirebaseAuthService instead of FirebaseAuth original one

--- mini_invoicer_app/lib/screens/home_screen.dart
+++ mini_invoicer_app/lib/screens/home_screen.dart
@@ -1,15 +1,15 @@
-import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:mini_invoicer_app/screens/company/company_detail_screen.dart';
 import 'package:mini_invoicer_app/screens/customer/customer_list_screen.dart';
 import 'package:mini_invoicer_app/screens/product/product_list_screen.dart';
+import 'package:mini_invoicer_app/services/firebase_auth_service.dart';
 import 'package:mini_invoicer_app/widgets/home_tile_widget.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    final auth = Provider.of<FirebaseAuth>(context);
+    final auth = Provider.of<FirebaseAuthService>(context);
     return Scaffold(
       appBar: AppBar(
         title: Text("Home"),
