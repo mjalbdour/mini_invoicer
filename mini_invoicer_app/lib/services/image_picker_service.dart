d5f5d779fce35e08d6018990bb5bd86db17de6ae
mjalbdour <mjalbdour@gmail.com>
2020-08-03 11:39:43 +0300

    created ImagePicker class w. ctor

--- mini_invoicer_app/lib/services/image_picker_service.dart
+++ mini_invoicer_app/lib/services/image_picker_service.dart
@@ -1,3 +1,12 @@
+import 'dart:io';
+
+import 'package:flutter/foundation.dart';
+import 'package:image_picker/image_picker.dart';
+
 class ImagePickerService {
   final ImagePicker imagePicker = ImagePicker();
+  Future<File> pickImage({@required ImageSource source}) async {
+    final pickedFile = await imagePicker.getImage(source: source);
+    return File(pickedFile.path);
+  }
 }
