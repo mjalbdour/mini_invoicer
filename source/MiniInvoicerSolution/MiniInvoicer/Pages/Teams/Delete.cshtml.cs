0314b5ed0a2d54ba72f6c818008cbe49daf5a29d
mjalbdour <mjalbdour@gmail.com>
2020-09-23 22:37:51 +0300

    ...teams to db context

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Pages/Teams/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MiniInvoicer.Data;
+using MiniInvoicer.Models;
+
+namespace MiniInvoicer.Pages.Teams
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MiniInvoicer.Data.ApplicationDbContext _context;
+
+        public DeleteModel(MiniInvoicer.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Team Team { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Team = await _context.Team.FirstOrDefaultAsync(m => m.TeamID == id);
+
+            if (Team == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Team = await _context.Team.FindAsync(id);
+
+            if (Team != null)
+            {
+                _context.Team.Remove(Team);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
