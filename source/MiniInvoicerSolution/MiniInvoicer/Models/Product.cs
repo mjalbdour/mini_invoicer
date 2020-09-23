06d2c0f99840660de5d0a1b5c5547e854fd0720e
mjalbdour <mjalbdour@gmail.com>
2020-09-23 12:32:54 +0300

    added product and pricing classes with annos.

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Product.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class Product
+    {
+        [Key]
+        public int ProductID { get; set; }
+
+        [ForeignKey("BrandID")]
+        public int BrandID { get; set; }
+
+        [ForeignKey("CompanyID")]
+        public int CompanyID { get; set; }
+
+        [ForeignKey("CategoryID")]
+        public int CategoryID { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Note { get; set; }
+
+        [Range(1, 1000)]
+        public int UnisPerPacket { get; set; }
+        [Range(1, 1000)]
+        public int PacketPerCase { get; set; }
+
+        [Range(1, 99999)]
+        public int UnitWeight { get; set; }
+
+        [Range(1, 99999)]
+        public int PacketWeight { get; set; }
+
+        [Range(1, 99999)]
+        public int CaseWeight { get; set; }
+
+        [Required]
+        [Column(TypeName = "money")]
+        public decimal Tax { get; set; }
+    }
+}
---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/ProductPricing.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class ProductPricing
+    {
+        [Key]
+        public int ProductPricingID { get; set; }
+
+        [ForeignKey("ProductID")]
+        public int ProductID { get; set; }
+
+        [ForeignKey("ProductPricingTypeID")]
+        public int ProductPricingTypeID { get; set; }
+
+        [Required]
+        [Range(0.1, 9999)]
+        public decimal Value { get; set; }
+    }
+}
