942ba850d1c91bb1fef57d66a30bbdd48f66d67f
mjalbdour <mjalbdour@gmail.com>
2020-09-23 12:58:55 +0300

    added Product Brand Class with anno.s

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/ProductBrand.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class ProductBrand
+    {
+        [Key]
+        public int ProductBrandID { get; set; }
+        
+        [ForeignKey("CountryID")]
+        public int ManufacturingCountryID { get; set; }
+        public string Name { get; set; }
+    }
+}
