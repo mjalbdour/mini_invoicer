c58258a79728285bded53873af618fa3831d6e55
mjalbdour <mjalbdour@gmail.com>
2020-09-23 11:48:57 +0300

    added department class with anno.

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Department.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class Department
+    {
+        [Key]
+        public int DepartmentID { get; set; }
+
+        [ForeignKey("CompanyID")]
+        public int CompanyID { get; set; }
+
+        [ForeignKey("EmployeeID")]
+        public int ManagerID { get; set; }
+
+
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
