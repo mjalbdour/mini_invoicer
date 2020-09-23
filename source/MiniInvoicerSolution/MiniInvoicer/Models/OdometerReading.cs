b36d5eef02337c8b4ddb86894f957cbe2805b708
mjalbdour <mjalbdour@gmail.com>
2020-09-23 22:53:29 +0300

    added odometerreading class with anno.s

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/OdometerReading.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class OdometerReading
+    {
+        [Key]
+        public int OdometerReadingID { get; set; }
+        
+        [ForeignKey("VehicleID")]
+        public int VehicleID { get; set; }
+
+        public int ReadingInKm { get; set; }
+    }
+}
