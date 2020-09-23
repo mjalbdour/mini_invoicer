f1afd80127bd831f8441c069039b60e43082c3e8
mjalbdour <mjalbdour@gmail.com>
2020-09-23 12:05:11 +0300

    added Company Type class with anno.s

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/CompanyType.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class CompanyType
+    {
+        [Key]
+        public int CompanyTypeID { get; set; }
+
+        [ForeignKey("TypeID")]
+        public int TypeID { get; set; }
+
+        [ForeignKey("CompanyID")]
+        public int CompanyID{ get; set; }
+
+    }
+}
