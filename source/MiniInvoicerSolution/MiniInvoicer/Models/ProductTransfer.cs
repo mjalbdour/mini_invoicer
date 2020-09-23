76ff29c87bc7d55ca7df7daca9b659596c6bbd9b
mjalbdour <mjalbdour@gmail.com>
2020-09-23 15:26:14 +0300

    added product transfer and transfered product classes

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/ProductTransfer.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class ProductTransfer
+    {
+        [Key]
+        public int ProductTransferID { get; set; }
+
+        [ForeignKey("WarehouseID")]
+        public int FromWarehouseID { get; set; }
+
+        [ForeignKey("WarehouseID")]
+        public int ToWarehouseID { get; set; }
+
+        public string Note { get; set; }
+
+        public decimal Value { get; set; }
+
+        public DateTime DateCreated { get; set; }
+    }
+}
---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/TransferedProduct.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class TransferedProduct
+    {
+        [Key]
+        public int TransferedProductID { get; set; }
+
+        [ForeignKey("ProductTransferID")]
+        public int ProductTransferID { get; set; }
+        
+        [ForeignKey("ProductID")]
+        public int ProductID { get; set; }
+
+        [Required]
+        public int Quantity { get; set; }
+    }
+}
