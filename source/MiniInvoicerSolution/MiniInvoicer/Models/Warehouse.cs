c6c9371438d2ed934b17ef37322b82818a6feade
mjalbdour <mjalbdour@gmail.com>
2020-09-23 12:21:24 +0300

    added Warehouse class with anno.s

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Warehouse.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class Warehouse
+    {
+        [Key]
+        public int WarehouseID { get; set; }
+
+        [ForeignKey("CompanyID")]
+        public int CompanyID { get; set; }
+
+        [ForeignKey("AddressID")]
+        public int AddressID { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        public int Number { get; set; }
+
+        public string Description { get; set; }
+
+    }
+}
