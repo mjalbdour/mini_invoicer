8a016f6eb243e468708feb224773e7845e7521b0
mjalbdour <mjalbdour@gmail.com>
2020-09-23 13:27:55 +0300

    added anno.s to Currency class

--- source/MiniInvoicerSolution/MiniInvoicer/Models/Currency.cs
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Currency.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -7,7 +8,15 @@ namespace MiniInvoicer.Models
 {
     public class Currency
     {
+        [Key]
         public int CurrencyID { get; set; }
+
+        [Required]
+        [MinLength(3)]
         public string Name { get; set; }
+
+        [Required]
+        [MinLength(3), StringLength(3)]
+        public string Acronym { get; set; }
     }
 }
