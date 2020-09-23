b0f90e77040e0324480b7774bd86dda4aa7432ad
mjalbdour <mjalbdour@gmail.com>
2020-09-23 11:46:09 +0300

    added Company Status class with annotations

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/CompanyStatus.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class CompanyStatus
+    {
+        [Key]
+        public int CompanyStatusID { get; set; }
+
+        [ForeignKey("StatusID")]
+        public int StatusID { get; set; }
+
+        [ForeignKey("CompanyID")]
+        public int CompanyID { get; set; }
+
+        [Timestamp]
+        [DataType(DataType.DateTime)]
+        public DateTime LastChanged { get; set; }
+    }
+}
