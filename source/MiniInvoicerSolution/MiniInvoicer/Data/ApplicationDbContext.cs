56d2d24c16c6b42c7460d8acd8f5a4611379ef85
mjalbdour <mjalbdour@gmail.com>
2020-09-23 16:52:51 +0300

    ...order to db context

--- source/MiniInvoicerSolution/MiniInvoicer/Data/ApplicationDbContext.cs
+++ source/MiniInvoicerSolution/MiniInvoicer/Data/ApplicationDbContext.cs
@@ -36,5 +36,6 @@ namespace MiniInvoicer.Data
         public DbSet<MiniInvoicer.Models.InvoiceProduct> InvoiceProduct { get; set; }
         public DbSet<MiniInvoicer.Models.Invoice> Invoice { get; set; }
         public DbSet<MiniInvoicer.Models.Neighborhood> Neighborhood { get; set; }
+        public DbSet<MiniInvoicer.Models.Order> Order { get; set; }
     }
 }
