c78d4504b78ca0e825331123abd583f83003eec0
mjalbdour <mjalbdour@gmail.com>
2020-09-23 12:03:24 +0300

    added generaic Type class with anno.s

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Type.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class Type
+    {
+        [Key]
+        public int TypeID { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Description { get; set; }
+    }
+}
