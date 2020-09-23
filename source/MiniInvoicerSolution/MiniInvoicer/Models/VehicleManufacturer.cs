546e2d3129cd087d02d34dce4cc76442a6834431
mjalbdour <mjalbdour@gmail.com>
2020-09-23 15:12:36 +0300

    added vehicle, manufacturer and status classes with anno.s

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/VehicleManufacturer.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class VehicleManufacturer
+    {
+        [Key]
+        public int VehicleManufacturerID { get; set; }
+
+        [ForeignKey("CountryID")]
+        public int CountryID { get; set; }
+
+        [Required]
+        [MinLength(2)]
+        public string Name { get; set; }
+    }
+}
