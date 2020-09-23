dd0820a42a2867e31c0d72caf82adf3a39a743c2
mjalbdour <mjalbdour@gmail.com>
2020-09-23 16:28:35 +0300

    ... assignedcompanytype to db context

--- source/MiniInvoicerSolution/MiniInvoicer/Data/ApplicationDbContext.cs
+++ source/MiniInvoicerSolution/MiniInvoicer/Data/ApplicationDbContext.cs
@@ -20,5 +20,6 @@ namespace MiniInvoicer.Data
         public DbSet<MiniInvoicer.Models.AreaDomain> AreaDomain { get; set; }
         public DbSet<MiniInvoicer.Models.Area> Area { get; set; }
         public DbSet<MiniInvoicer.Models.AssignedCompanyStatus> AssignedCompanyStatus { get; set; }
+        public DbSet<MiniInvoicer.Models.AssignedCompanyType> AssignedCompanyType { get; set; }
     }
 }
---
+++ source/MiniInvoicerSolution/MiniInvoicer/Pages/AssignedCompanyTypes/Details.cshtml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MiniInvoicer.Data;
+using MiniInvoicer.Models;
+
+namespace MiniInvoicer.Pages.AssignedCompanyTypes
+{
+    public class DetailsModel : PageModel
+    {
+        private readonly MiniInvoicer.Data.ApplicationDbContext _context;
+
+        public DetailsModel(MiniInvoicer.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        public AssignedCompanyType AssignedCompanyType { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            AssignedCompanyType = await _context.AssignedCompanyType.FirstOrDefaultAsync(m => m.AssignedCompanyTypeID == id);
+
+            if (AssignedCompanyType == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+    }
+}
