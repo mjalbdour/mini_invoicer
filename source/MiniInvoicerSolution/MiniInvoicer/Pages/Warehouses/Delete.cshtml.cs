e074b58f5a0455467f9f454adcbabee7928906f1
mjalbdour <mjalbdour@gmail.com>
2020-09-23 22:46:08 +0300

    ...warehouse to db context

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Pages/Warehouses/Delete.cshtml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.EntityFrameworkCore;
+using MiniInvoicer.Data;
+using MiniInvoicer.Models;
+
+namespace MiniInvoicer.Pages.Warehouses
+{
+    public class DeleteModel : PageModel
+    {
+        private readonly MiniInvoicer.Data.ApplicationDbContext _context;
+
+        public DeleteModel(MiniInvoicer.Data.ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [BindProperty]
+        public Warehouse Warehouse { get; set; }
+
+        public async Task<IActionResult> OnGetAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Warehouse = await _context.Warehouse.FirstOrDefaultAsync(m => m.WarehouseID == id);
+
+            if (Warehouse == null)
+            {
+                return NotFound();
+            }
+            return Page();
+        }
+
+        public async Task<IActionResult> OnPostAsync(int? id)
+        {
+            if (id == null)
+            {
+                return NotFound();
+            }
+
+            Warehouse = await _context.Warehouse.FindAsync(id);
+
+            if (Warehouse != null)
+            {
+                _context.Warehouse.Remove(Warehouse);
+                await _context.SaveChangesAsync();
+            }
+
+            return RedirectToPage("./Index");
+        }
+    }
+}
