023636bcf077581e51af5b6164fa1e15d84ad1ea
mjalbdour <mjalbdour@gmail.com>
2020-09-25 15:22:29 +0300

    fixed areaID

--- source/MiniInvoicerSolution/MiniInvoicer/Models/Neighborhood.cs
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Neighborhood.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
 using System.Linq;
 using System.Threading.Tasks;
 
@@ -10,6 +11,10 @@ namespace MiniInvoicer.Models
     {
         [Key]
         public int NeighborhoodID { get; set; }
+
+        [ForeignKey("AreaID")]
+        public int AreaID { get; set; }
+        public Area Area { get; set; }
 
         [Required]
         public string Name { get; set; }
