128b4015c8cd9d0b9061bf1cb08673e711bd4d1f
mjalbdour <mjalbdour@gmail.com>
2020-09-25 14:38:48 +0300

    added refs

--- source/MiniInvoicerSolution/MiniInvoicer/Models/Address.cs
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Address.cs
@@ -14,27 +14,34 @@ namespace MiniInvoicer.Models
 
         [ForeignKey("StreetID")]
         public int StreetID { get; set; }
+        public Street Street { get; set; }
 
         [ForeignKey("AreaID")]
         public int AreaID { get; set; }
+        public Area Area { get; set; }
 
         [ForeignKey("NeighborhoodID")]
         public int NeighborhoodID { get; set; }
+        public Neighborhood Neighborhood { get; set; }
 
         [ForeignKey("CityID")]
         public int CityID { get; set; }
+        public City City { get; set; }
 
         [ForeignKey("CountryID")]
         public int CountryID { get; set; }
+        public Country Country { get; set; }
 
         [ForeignKey("ZipcodeID")]
         public int ZipcodeID { get; set; }
+        public Zipcode Zipcode { get; set; }
 
         [ForeignKey("HolderID")]
         public int HolderID { get; set; }
 
         [ForeignKey("GovernorateID")]
         public int GovernorateID { get; set; }
+        public Governorate Governorate { get; set; }
 
         public int BuildingNumber { get; set; }
         public string BuildingName { get; set; }
