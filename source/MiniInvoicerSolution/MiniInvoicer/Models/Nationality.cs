3d0b7fc648abb7e963daa023ee44983a6a825fb5
mjalbdour <mjalbdour@gmail.com>
2020-09-25 14:49:43 +0300

    added nationality class with anno.s

---
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/Nationality.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicer.Models
+{
+    public class Nationality
+    {
+        [Key]
+        public int NationalityID { get; set; }
+
+        [Required]
+        [MinLength(3)]
+        public string Name { get; set; }
+    }
+}
