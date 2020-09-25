6591bde562f777546cf8ede9760d622751a22264
mjalbdour <mjalbdour@gmail.com>
2020-09-25 15:21:14 +0300

    product pricing fix

--- source/MiniInvoicerSolution/MiniInvoicer/Models/InvoiceProduct.cs
+++ source/MiniInvoicerSolution/MiniInvoicer/Models/InvoiceProduct.cs
@@ -14,9 +14,11 @@ namespace MiniInvoicer.Models
 
         [ForeignKey("InvoiceID")]
         public int InvoiceID { get; set; }
+        public Invoice Invoice { get; set; }
 
         [ForeignKey("ProductPricingID")]
         public int ProductPricingID { get; set; }
+        public ProductPricing ProductPricing { get; set; }
 
         [Required]
         public int ProductQuantity { get; set; }
