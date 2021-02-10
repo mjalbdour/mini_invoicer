4d84a51a01f64c352f789f6c2a564754ac885aa4
mjalbdour <mjalbdour@gmail.com>
2021-02-10 11:52:10 +0200

    last commit for the mvc dapper project

---
+++ source/MiniInvoicerMVCSolution/MiniInvoicerMVC/Controllers/HomeController.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MiniInvoicerMVC.Controllers
+{
+    public class HomeController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
---
+++ source/MiniInvoicerMVCSolution/MiniInvoicerMVC/Controllers/InvoicesController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Reflection.Metadata.Ecma335;
+using System.Threading.Tasks;
+
+namespace MiniInvoicerMVC.Controllers
+{
+    public class InvoicesController : Controller
+    {
+        public IActionResult Index() => View();
+    }
+}
