51eb67f4b0ab6ebf71844c949b763992392e6a57
mjalbdour <mjalbdour@gmail.com>
2021-02-13 12:52:11 +0200

    generated PricingType Initial Migration

---
+++ src/MiniInvoicer.Server/Migrations/20210213105055_PricingTypeInitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniInvoicer.Server.Migrations
+{
+    public partial class PricingTypeInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
