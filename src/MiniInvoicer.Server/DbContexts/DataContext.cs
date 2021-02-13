1ae94d65eb39111383829f2d3abe905e81d0606b
mjalbdour <mjalbdour@gmail.com>
2021-02-13 12:24:40 +0200

    Created DbContexts folder and DataContext class

---
+++ src/MiniInvoicer.Server/DbContexts/DataContext.cs
@@ -0,0 +1,11 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace MiniInvoicer.Server.DbContexts
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions<DataContext> options) : base(options)
+        {
+        }
+    }
+}
