4e7546a6d68bf3600a77eef492de51ff9abe797d
mjalbdour <mjalbdour@gmail.com>
2021-02-13 19:26:14 +0200

    created identity context class

---
+++ src/MiniInvoicer.Server/DbContexts/IdentityContext.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Identity.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore;
+
+namespace MiniInvoicer.Server.DbContexts
+{
+    public class IdentityContext : IdentityDbContext<IdentityUser>
+    {
+        public IdentityContext(DbContextOptions<IdentityContext> options) : base(options)
+        {
+        }
+    }
+}
