ed7b5e24009217c1cd4b264d7d0e54139876c45f
mjalbdour <mjalbdour@gmail.com>
2021-02-13 12:56:08 +0200

    generated ProductPricing Initial Migration

---
+++ src/MiniInvoicer.Server/Migrations/20210213105449_ProductPricingInitial.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace MiniInvoicer.Server.Migrations
+{
+    public partial class ProductPricingInitial : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
