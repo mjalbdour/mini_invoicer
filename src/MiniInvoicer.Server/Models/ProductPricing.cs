8bf71238808fc3d0dc9b37069e52a5bf51476fd3
mjalbdour <mjalbdour@gmail.com>
2021-02-13 12:55:24 +0200

    Created ProductPricing Model

---
+++ src/MiniInvoicer.Server/Models/ProductPricing.cs
@@ -0,0 +1,10 @@
+﻿namespace MiniInvoicer.Server.Models
+{
+    public class ProductPricing
+    {
+        public int Id { get; set; }
+        public int ProductId { get; set; }
+        public int PricingTypeId { get; set; }
+        public decimal Value { get; set; }
+    }
+}
