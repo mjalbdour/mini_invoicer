3c1afc154a977bfc9923c891b4abaf5ba89909d7
mjalbdour <mjalbdour@gmail.com>
2021-02-14 06:35:22 +0200

    created initial users controller, no auth yet...

---
+++ src/MiniInvoicer.Server/Controllers/UsersController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections.Generic;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniInvoicer.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class UsersController : ControllerBase
+    {
+        private readonly UserManager<IdentityUser> _userManager;
+
+        public UsersController(UserManager<IdentityUser> _userManager)
+        {
+            this._userManager = _userManager;
+        }
+
+        [HttpGet]
+        public async Task<IEnumerable<IdentityUser>> GetUsers()
+        {
+            return await Task.FromResult(_userManager.Users);
+        }
+    }
+}
