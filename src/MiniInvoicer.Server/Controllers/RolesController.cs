64bb0bd47b8db25931d1f7e1508c42b405dcca9f
mjalbdour <mjalbdour@gmail.com>
2021-02-14 15:15:40 +0200

    added get roles list logic

---
+++ src/MiniInvoicer.Server/Controllers/RolesController.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+
+namespace MiniInvoicer.Server.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class RolesController : ControllerBase
+    {
+        private readonly RoleManager<IdentityRole> _roleManager;
+
+        public RolesController(RoleManager<IdentityRole> roleManager)
+        {
+            _roleManager = roleManager;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<IEnumerable<IdentityRole>>> GetRoles()
+        {
+            return await Task.FromResult(_roleManager.Roles.ToList());
+        }
+    }
+}
