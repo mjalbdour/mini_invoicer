830d987936059462384d03a23933027e9e96321f
mjalbdour <mjalbdour@gmail.com>
2021-02-14 12:13:42 +0200

    added edit user logic

--- src/MiniInvoicer.Server/Controllers/UsersController.cs
+++ src/MiniInvoicer.Server/Controllers/UsersController.cs
@@ -25,12 +25,24 @@ namespace MiniInvoicer.Server.Controllers
             return await Task.FromResult(_userManager.Users.ToList());
         }
 
+        [HttpGet("{id}")]
+        public async Task<ActionResult<EditUserModel>> GetUser([FromRoute] string id)
+        {
+            var user = await _userManager.FindByIdAsync(id);
+            if (user == null)
+            {
+                return NotFound(id);
+            }
+
+            return Ok(new EditUserModel {Id = id, Email = user.Email, Username = user.UserName});
+        }
+
         [HttpPost]
-        public async Task<IActionResult> CreateUser(CreateUserModel userModel)
+        public async Task<IActionResult> CreateUser([FromBody] EditUserModel userModel)
         {
             if (ModelState.IsValid)
             {
-                IdentityUser user = new IdentityUser { UserName = userModel.Username, Email = userModel.Email};
+                IdentityUser user = new IdentityUser {UserName = userModel.Username, Email = userModel.Email};
                 IdentityResult result = await _userManager.CreateAsync(user, userModel.Password);
                 if (result.Succeeded)
                 {
@@ -46,6 +58,42 @@ namespace MiniInvoicer.Server.Controllers
             }
 
             return BadRequest();
+        }
+
+        [HttpPut]
+        public async Task<IActionResult> EditUser([FromBody] EditUserModel userModel)
+        {
+            if (!ModelState.IsValid)
+            {
+                Console.WriteLine(ModelState.ErrorCount);
+                return BadRequest();
+            }
+
+            IdentityUser user = await _userManager.FindByIdAsync(userModel.Id);
+            if (user != null)
+            {
+                user.Email = userModel.Email;
+                user.UserName = userModel.Username;
+                IdentityResult result = await _userManager.UpdateAsync(user);
+                if (result.Succeeded)
+                {
+                    await _userManager.RemovePasswordAsync(user);
+                    result = await _userManager.AddPasswordAsync(user, userModel.Password);
+                    if (result.Succeeded)
+                    {
+                        return Accepted();
+                    }
+
+                    var passwordChangeErrors = result.Errors.Select(x => x.Description);
+
+                    return BadRequest(passwordChangeErrors);
+                }
+                var emailusernameErrors = result.Errors.Select(x => x.Description);
+
+                return BadRequest(emailusernameErrors);
+            }
+
+            return NotFound(userModel.Id);
         }
     }
 }
