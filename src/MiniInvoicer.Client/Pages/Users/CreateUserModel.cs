8417a7fcfcfc67c4b45b4ea1ede625b9f396726d
mjalbdour <mjalbdour@gmail.com>
2021-02-14 08:48:29 +0200

    created create user model

---
+++ src/MiniInvoicer.Client/Pages/Users/CreateUserModel.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace MiniInvoicer.Client.Pages.Users
+{
+    public class CreateUserModel
+    {
+        [Required]
+        [StringLength(16, MinimumLength = 3)]
+        public string Username { get; set; }
+
+        [Required]
+        [EmailAddress]
+        public string Email { get; set; }
+
+        [Required]
+        [PasswordPropertyText]
+        [StringLength(32, MinimumLength = 8)]
+        public string Password { get; set; }
+    }
+}
