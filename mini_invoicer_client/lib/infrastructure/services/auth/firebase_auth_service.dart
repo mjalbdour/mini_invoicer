b0342f4ed8f58bc434f1bd8641f10cb3b0370fca
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 08:08:28 +0200

    created initial fbase auth service class

---
+++ mini_invoicer_client/lib/infrastructure/services/auth/firebase_auth_service.dart
@@ -0,0 +1,6 @@
+import 'package:firebase_auth/firebase_auth.dart';
+
+class FirebaseAuthenticationService {
+  final FirebaseAuth _firebaseAuth;
+  FirebaseAuthenticationService(this._firebaseAuth);
+}
