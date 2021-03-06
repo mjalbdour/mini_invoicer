62cdecfefc46a4132a6923dde6763f8dbad1f230
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 18:48:42 +0200

    dynamic form screen instead of 2 seperate signin/up screens

--- mini_invoicer_client/lib/ui/screens/auth/auth_form_screen.dart
+++ mini_invoicer_client/lib/ui/screens/auth/auth_form_screen.dart
@@ -2,31 +2,42 @@ import 'package:flutter/material.dart';
 import 'package:mini_invoicer_client/infrastructure/services/auth/firebase_auth_service.dart';
 import 'package:provider/provider.dart';
 
-class SignInScreen extends StatelessWidget {
+class AuthFormScreen extends StatefulWidget {
   // ROUTE
-  static const String Route = '/auth/signin';
+  static const String Route = '/auth/form';
 
-  // Form Key
+  @override
+  _AuthFormScreenState createState() => _AuthFormScreenState();
+}
+
+class _AuthFormScreenState extends State<AuthFormScreen> {
+  FormType formType = FormType.SignIn;
+
   final formKey = GlobalKey<FormState>();
 
-  // Form fields Editing CONTROLLERS
   final TextEditingController emailController = TextEditingController();
+
   final TextEditingController passwordController = TextEditingController();
 
   @override
   Widget build(BuildContext context) {
+    print(formType);
     return Scaffold(
       body: Center(
         child: Form(
+          key: formKey,
           child: ListView(
             padding: EdgeInsets.all(16.0),
             shrinkWrap: true,
             children: [
               Center(
                 child: Text(
-                  'Sign in',
+                  formType == FormType.SignIn ? 'Sign in' : 'Sign up',
                   style: TextStyle(fontSize: 48.0),
                 ),
+              ),
+              SizedBox(
+                height: 24.0,
               ),
               TextFormField(
                 keyboardType: TextInputType.emailAddress,
@@ -50,20 +61,39 @@ class SignInScreen extends StatelessWidget {
                     child: Text("reset"),
                   ),
                   ElevatedButton.icon(
-                    onPressed: () => {
-                      context
-                          .read<FirebaseAuthenticationService>()
-                          .signInWithEmailAndPassword(
-                            email: emailController.text.trim(),
-                            password: passwordController.text.trim(),
-                          ),
-                    },
+                    onPressed: () => formType == FormType.SignIn
+                        ? context
+                            .read<FirebaseAuthenticationService>()
+                            .signInWithEmailAndPassword(
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim(),
+                            )
+                        : context
+                            .read<FirebaseAuthenticationService>()
+                            .signUpWithEmailAndPassword(
+                              email: emailController.text.trim(),
+                              password: passwordController.text.trim(),
+                            ),
                     icon: Icon(Icons.arrow_forward_rounded),
                     label: Text("next"),
                   ),
                 ],
               ),
-              // TextButton(onPressed: () => null, child: Text("Create Account")),
+              SizedBox(
+                height: 24.0,
+              ),
+              TextButton(
+                onPressed: () => setState(
+                  () => formType == FormType.SignIn
+                      ? formType = FormType.SignUp
+                      : formType = FormType.SignIn,
+                ),
+                child: Text(
+                  formType == FormType.SignIn
+                      ? "Sign up instead"
+                      : "Sign in instead",
+                ),
+              ),
             ],
           ),
         ),
@@ -71,3 +101,5 @@ class SignInScreen extends StatelessWidget {
     );
   }
 }
+
+enum FormType { SignIn, SignUp }
