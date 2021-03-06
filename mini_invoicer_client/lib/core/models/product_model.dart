8393594205a930be9f7e4917b28c37f1483a73d7
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 20:59:00 +0200

    +product model

---
+++ mini_invoicer_client/lib/core/models/product_model.dart
@@ -0,0 +1,44 @@
+class Product {
+  String id;
+  String name;
+  String description;
+  int weight;
+
+  String brandId;
+  String vendorId;
+
+  Product(
+      {this.id,
+      this.name,
+      this.description,
+      this.weight,
+      this.brandId,
+      this.vendorId});
+
+  factory Product.fromJson(String id, Map<String, dynamic> json) {
+    return Product(
+      id: id,
+      name: json['name'] as String,
+      description: json['description'] as String,
+      weight: json['weight'] as int,
+      brandId: json['brandId'] as String,
+      vendorId: json['vendorId'] as String,
+    );
+  }
+
+  Map<String, dynamic> toJson() => {
+        'name': this.name,
+        'description': this.description,
+        'weight': this.weight,
+        'brandId': this.brandId,
+        'vendorId': this.vendorId,
+      };
+  Map<String, dynamic> toJsonWithId() => {
+        'id': this.id,
+        'name': this.name,
+        'description': this.description,
+        'weight': this.weight,
+        'brandId': this.brandId,
+        'vendorId': this.vendorId,
+      };
+}
