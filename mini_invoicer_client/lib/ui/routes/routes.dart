a0b7418a4e92ef690ea5770fdddfd8c810ae3a9e
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 18:48:10 +0200

    -signin/up route entries, +form screen route entry

--- mini_invoicer_client/lib/ui/routes/routes.dart
+++ mini_invoicer_client/lib/ui/routes/routes.dart
@@ -1,6 +1,10 @@
 import 'package:flutter/widgets.dart';
+
+// Screens | Wrappers
 import 'package:mini_invoicer_client/ui/common/auth_wrapper.dart';
+import 'package:mini_invoicer_client/ui/screens/auth/auth_form_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
   AuthWrapper.Route: (context) => AuthWrapper(),
+  AuthFormScreen.Route: (context) => AuthFormScreen(),
 };
