2380b6fe04abc722b9a915a90b73d41f69cfe59a
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 22:17:24 +0200

    + product_pricing model

---
+++ mini_invoicer_client/lib/core/models/product_pricing_model.dart
@@ -0,0 +1,49 @@
+class ProductPricing {
+  final String id;
+  final double value;
+  final int salesTax;
+  final String description;
+  final String productId;
+  final String pricingId;
+  final String currencyId;
+
+  const ProductPricing({
+    this.id,
+    this.value,
+    this.salesTax,
+    this.description,
+    this.productId,
+    this.pricingId,
+    this.currencyId,
+  });
+
+  factory ProductPricing.fromJson(String id, Map<String, dynamic> json) =>
+      ProductPricing(
+        id: id,
+        value: json['value'] as double,
+        salesTax: json['salesTax'] as int,
+        description: json['description'],
+        productId: json['productId'],
+        pricingId: json['pricingId'],
+        currencyId: json['currencyId'],
+      );
+
+  Map<String, dynamic> toJson() => {
+        'value': this.value,
+        'salesTax': this.salesTax,
+        'description': this.description,
+        'productId': this.productId,
+        'pricingId': this.pricingId,
+        'currencyId': this.currencyId,
+      };
+
+  Map<String, dynamic> toJsonWithId(String id) => {
+        'id': id,
+        'value': this.value,
+        'salesTax': this.salesTax,
+        'description': this.description,
+        'productId': this.productId,
+        'pricingId': this.pricingId,
+        'currencyId': this.currencyId,
+      };
+}
