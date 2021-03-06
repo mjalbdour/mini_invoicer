6503f823b88c12a9ddbef9998fd11f8625232213
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 08:58:49 +0200

    +multiprovider for auth service & auth state changes stream, routes and initial route to mat.app

--- mini_invoicer_client/lib/main.dart
+++ mini_invoicer_client/lib/main.dart
@@ -1,5 +1,11 @@
+import 'package:firebase_auth/firebase_auth.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'package:flutter/material.dart';
+import 'package:mini_invoicer_client/ui/common/auth_wrapper.dart';
+import 'package:mini_invoicer_client/ui/routes/routes.dart';
+import 'package:provider/provider.dart';
+
+import 'infrastructure/services/auth/firebase_auth_service.dart';
 
 Future<void> main() async {
   WidgetsFlutterBinding.ensureInitialized();
@@ -10,12 +16,21 @@ Future<void> main() async {
 class MiniInvoicer extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: "Mini Invoicer",
-      home: Scaffold(
-        body: Center(
-          child: Text("Mini Invoicer"),
+    return MultiProvider(
+      providers: [
+        Provider<FirebaseAuthenticationService>(
+          create: (_) => FirebaseAuthenticationService(FirebaseAuth.instance),
         ),
+        StreamProvider(
+          initialData: null,
+          create: (context) =>
+              context.read<FirebaseAuthenticationService>().authStateChanges,
+        )
+      ],
+      child: MaterialApp(
+        title: 'Mini Invoicer',
+        routes: routes,
+        initialRoute: AuthWrapper.Route,
       ),
     );
   }
