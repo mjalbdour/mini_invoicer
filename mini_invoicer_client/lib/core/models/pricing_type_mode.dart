0e9b82f51be63c49188668bd69a4cbfe1552c266
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 21:20:38 +0200

    + pricing_type model

---
+++ mini_invoicer_client/lib/core/models/pricing_type_mode.dart
@@ -0,0 +1,24 @@
+class PricingType {
+  final String id;
+  final String name;
+  final String description;
+
+  const PricingType({this.id, this.name, this.description});
+
+  factory PricingType.fromJson(String id, Map<String, dynamic> json) =>
+      PricingType(
+          id: id,
+          name: json['name'] as String,
+          description: json['description'] as String);
+
+  Map<String, dynamic> toJson() => {
+        'name': this.name,
+        'description': this.description,
+      };
+
+  Map<String, dynamic> toJsonWithId(String id) => {
+        id: id,
+        'name': this.name,
+        'description': this.description,
+      };
+}
