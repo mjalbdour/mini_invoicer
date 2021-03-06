f388a96d2b1b0d9a925387132314a677574eef26
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 08:21:19 +0200

    added signin, signup methods and authstate stream getter

--- mini_invoicer_client/lib/infrastructure/services/auth/firebase_auth_service.dart
+++ mini_invoicer_client/lib/infrastructure/services/auth/firebase_auth_service.dart
@@ -3,4 +3,28 @@ import 'package:firebase_auth/firebase_auth.dart';
 class FirebaseAuthenticationService {
   final FirebaseAuth _firebaseAuth;
   FirebaseAuthenticationService(this._firebaseAuth);
+
+  Stream<User> get authStateChanges => _firebaseAuth.authStateChanges();
+
+  Future<String> signInWithEmailAndPassword(
+      {String email, String password}) async {
+    try {
+      await _firebaseAuth.signInWithEmailAndPassword(
+          email: email, password: password);
+      return 'Signed In';
+    } on FirebaseAuthException catch (exception) {
+      return exception.message;
+    }
+  }
+
+  Future<String> signUpWithEmailAndPassword(
+      {String email, String password}) async {
+    try {
+      await _firebaseAuth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      return 'Signed Up';
+    } on FirebaseAuthException catch (exception) {
+      return exception.message;
+    }
+  }
 }
