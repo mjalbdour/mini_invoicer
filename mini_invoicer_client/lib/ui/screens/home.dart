d74e859b533e46bcd56d8d74e14afadb515a4728
MJALBDOUR <mjalbdour@gmail.com>
2021-03-06 09:51:58 +0200

    + sign out action on app bar

--- mini_invoicer_client/lib/ui/screens/home.dart
+++ mini_invoicer_client/lib/ui/screens/home.dart
@@ -1,9 +1,20 @@
 import 'package:flutter/material.dart';
+import 'package:mini_invoicer_client/infrastructure/services/auth/firebase_auth_service.dart';
+import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      appBar: AppBar(
+        actions: [
+          IconButton(
+            icon: Icon(Icons.exit_to_app_outlined),
+            onPressed: () =>
+                context.read<FirebaseAuthenticationService>().signOut(),
+          ),
+        ],
+      ),
       body: Center(
         child: Text("home screen"),
       ),
