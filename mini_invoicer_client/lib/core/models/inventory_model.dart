4240b83d0d24657b4f9c36dfd281609bf1c86459
MJALBDOUR <mjalbdour@gmail.com>
2021-03-07 22:09:12 +0200

    ++lastUpdated DateTime

--- mini_invoicer_client/lib/core/models/inventory_model.dart
+++ mini_invoicer_client/lib/core/models/inventory_model.dart
@@ -2,11 +2,13 @@ class Inventory {
   final String id;
   final String name;
   final String ownerId;
+  final DateTime lastUpdated;
   final String inventoryTypeId;
 
   const Inventory({
     this.id,
     this.name,
+    this.lastUpdated,
     this.ownerId,
     this.inventoryTypeId,
   });
@@ -14,12 +16,14 @@ class Inventory {
   factory Inventory.fromJson(String id, Map<String, dynamic> json) => Inventory(
         id: id,
         name: json['name'] as String,
+        lastUpdated: json['lastUpdated'].toDate() as DateTime,
         ownerId: json['ownerId'] as String,
         inventoryTypeId: json['inventoryTypeId'] as String,
       );
 
   Map<String, dynamic> toJson() => {
         'name': this.name,
+        'lastUpdated': this.lastUpdated,
         'ownerId': this.ownerId,
         'inventoryTypeId': this.inventoryTypeId,
       };
@@ -27,6 +31,7 @@ class Inventory {
   Map<String, dynamic> toJsonWithId() => {
         'id': this.id,
         'name': this.name,
+        'lastUpdated': this.lastUpdated,
         'ownerId': this.ownerId,
         'inventoryTypeId': this.inventoryTypeId,
       };
