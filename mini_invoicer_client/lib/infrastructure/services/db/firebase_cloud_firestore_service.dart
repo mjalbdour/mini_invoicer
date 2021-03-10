8087bc2764c44ea3c53f5241431205aa57de7497
MJALBDOUR <mjalbdour@gmail.com>
2021-03-10 11:13:54 +0200

    ++generic queries (simple)

--- mini_invoicer_client/lib/infrastructure/services/db/firebase_cloud_firestore_service.dart
+++ mini_invoicer_client/lib/infrastructure/services/db/firebase_cloud_firestore_service.dart
@@ -5,22 +5,34 @@ class FirebaseCloudFirestoreService {
   final FirebaseFirestore _db;
   FirebaseCloudFirestoreService(this._db);
 
-  // Product QUERY
-  Stream<Product> streamProduct(String id) => _db
-      .collection('products')
-      .doc(id)
-      .snapshots()
-      .map((snapshot) => Product.fromJson(id, snapshot.data()));
+  // GENERIC QUERIES
+  // SINGLE
+  Stream<T> _streamSingle<T>(String id, String collectionPath,
+          T fromJson(String id, Map<String, dynamic> json)) =>
+      _db
+          .collection(collectionPath)
+          .doc(id)
+          .snapshots()
+          .map((snapshot) => fromJson(id, snapshot.data()));
 
-  Stream<Set<Product>> streamProducts() =>
-      _db.collection('products').snapshots().map(
-            (snapshot) => snapshot.docs
-                .map(
-                  (doc) => Product.fromJson(
-                    doc.id,
-                    doc.data(),
-                  ),
-                )
-                .toSet(),
-          );
+  // MULTIPLE
+  Stream<List<T>> _streamMultiple<T>(String collectionPath,
+          T fromJson(String id, Map<String, dynamic> json)) =>
+      _db.collection(collectionPath).snapshots().map((snapshot) => snapshot.docs
+          .map(
+            (doc) => fromJson(
+              doc.id,
+              doc.data(),
+            ),
+          )
+          .toList());
+
+  // Product QUERY
+  // SINGLE
+  Stream<Product> streamProduct(String id) => _streamSingle<Product>(
+      id, 'products', (id, json) => Product.fromJson(id, json));
+
+  // MULTIPLE
+  Stream<List<Product>> streamProducts() => _streamMultiple<Product>(
+      'products', (id, json) => Product.fromJson(id, json));
 }
