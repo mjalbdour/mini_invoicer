babf2e018de7a1f1dfec71bcc9eb563e383f4c95
MJALBDOUR <mjalbdour@gmail.com>
2021-03-10 23:58:28 +0200

    +inventory types screen

---
+++ mini_invoicer_client/lib/ui/screens/inventory_type/inventory_types_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:mini_invoicer_client/core/models/inventory_type_model.dart';
+import 'package:mini_invoicer_client/infrastructure/services/db/firebase_cloud_firestore_service.dart';
+import "package:provider/provider.dart";
+
+class InventoriesScreen extends StatelessWidget {
+  static const String ROUTE = "/inventorytypes";
+  @override
+  Widget build(BuildContext context) {
+    final _inventoriesStream =
+        context.watch<FirebaseCloudFirestoreService>().streamInventories();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inventory Types"),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<List<InventoryType>>(
+        initialData: [],
+        stream: _inventoriesStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Oops! Something went wrong"),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var _inventories = snapshot.data;
+          if (_inventories.isEmpty) {
+            return Center(
+              child: Text("No inventory types found"),
+            );
+          }
+
+          return GridView.builder(
+            padding: EdgeInsets.all(16.0),
+            itemCount: _inventories.length,
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        InventoryTypeScreen(_inventories[index].id))),
+                child: Container(
+                  height: 100.0,
+                  child: Card(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 0,
+                        width: 0,
+                      ),
+                      Text("${_inventories[index].name}"),
+                      Container(
+                        color: Colors.black,
+                        padding: EdgeInsets.all(2.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Icon(
+                              Icons.ac_unit_outlined,
+                              color: Colors.white,
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
