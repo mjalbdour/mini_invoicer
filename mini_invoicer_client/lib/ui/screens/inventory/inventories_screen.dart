8eb19993e22370c49ddbccfb31166f427a23ffaa
MJALBDOUR <mjalbdour@gmail.com>
2021-03-10 23:30:36 +0200

    +inventory list & detail screens

---
+++ mini_invoicer_client/lib/ui/screens/inventory/inventories_screen.dart
@@ -0,0 +1,84 @@
+import 'package:flutter/material.dart';
+import 'package:mini_invoicer_client/core/models/inventory_model.dart';
+import 'package:mini_invoicer_client/infrastructure/services/db/firebase_cloud_firestore_service.dart';
+import "package:provider/provider.dart";
+
+class InventoriesScreen extends StatelessWidget {
+  static const String ROUTE = "/inventories";
+  @override
+  Widget build(BuildContext context) {
+    final _inventoriesStream =
+        context.watch<FirebaseCloudFirestoreService>().streamInventories();
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Inventories"),
+        centerTitle: true,
+      ),
+      body: StreamBuilder<List<Inventory>>(
+        initialData: [],
+        stream: _inventoriesStream,
+        builder: (context, snapshot) {
+          if (snapshot.hasError) {
+            return Center(
+              child: Text("Oops! Something went wrong"),
+            );
+          }
+
+          if (snapshot.connectionState == ConnectionState.waiting) {
+            return Center(
+              child: CircularProgressIndicator(),
+            );
+          }
+
+          var _inventories = snapshot.data;
+          if (_inventories.isEmpty) {
+            return Center(
+              child: Text("No inventories found"),
+            );
+          }
+
+          return GridView.builder(
+            padding: EdgeInsets.all(16.0),
+            itemCount: _inventories.length,
+            gridDelegate:
+                SliverGridDelegateWithFixedCrossAxisCount(crossAxisCount: 2),
+            itemBuilder: (context, index) {
+              return GestureDetector(
+                onTap: () => Navigator.of(context).push(MaterialPageRoute(
+                    builder: (context) =>
+                        InventoryScreen(_inventories[index].id))),
+                child: Container(
+                  height: 100.0,
+                  child: Card(
+                      child: Column(
+                    mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                    children: [
+                      SizedBox(
+                        height: 0,
+                        width: 0,
+                      ),
+                      Text("${_inventories[index].name}"),
+                      Container(
+                        color: Colors.black,
+                        padding: EdgeInsets.all(2.0),
+                        child: Row(
+                          mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                          children: [
+                            Text(
+                              "${_inventories[index].inventoryTypeId}",
+                              style: TextStyle(color: Colors.white),
+                            ),
+                          ],
+                        ),
+                      ),
+                    ],
+                  )),
+                ),
+              );
+            },
+          );
+        },
+      ),
+    );
+  }
+}
---
+++ mini_invoicer_client/lib/ui/screens/inventory/inventory_screen.dart
@@ -0,0 +1,64 @@
+import 'package:flutter/material.dart';
+import 'package:mini_invoicer_client/core/models/inventory_model.dart';
+import 'package:mini_invoicer_client/infrastructure/services/db/firebase_cloud_firestore_service.dart';
+import 'package:provider/provider.dart';
+
+class InventoryScreen extends StatelessWidget {
+  String _id;
+  String _route;
+  InventoryScreen(String id) {
+    this._id = id;
+    this._route = "/inventories/$id";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Stream<Inventory> _inventoryStream =
+        context.watch<FirebaseCloudFirestoreService>().streamInventory(_id);
+    return StreamBuilder<Inventory>(
+      initialData: Inventory(),
+      stream: _inventoryStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                children: [
+                  Text("Oops! Something went wrong"),
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_outlined),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        Inventory _inventory = snapshot.data;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("${_inventory.name}"),
+          ),
+          body: ListView(
+            padding: EdgeInsets.all(16.0),
+            shrinkWrap: true,
+            children: [
+              Text("${_inventory.ownerId}"),
+              Text("${_inventory.inventoryTypeId}"),
+              Text("${_inventory.lastUpdated}"),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
