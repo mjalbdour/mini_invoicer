72d3a557f0efe63bb2f7949216cf272030fa9348
MJALBDOUR <mjalbdour@gmail.com>
2021-03-11 11:44:27 +0200

    +inventory_type screen (detail)

---
+++ mini_invoicer_client/lib/ui/screens/inventory_type/inventory_type_screen.dart
@@ -0,0 +1,62 @@
+import 'package:flutter/material.dart';
+import 'package:mini_invoicer_client/core/models/inventory_type_model.dart';
+import 'package:mini_invoicer_client/infrastructure/services/db/firebase_cloud_firestore_service.dart';
+import 'package:provider/provider.dart';
+
+class InventoryTypeScreen extends StatelessWidget {
+  String _id;
+  String _route;
+  InventoryTypeScreen(String id) {
+    this._id = id;
+    this._route = "/inventorytypes/$id";
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    Stream<InventoryType> _inventoryTypeStream =
+        context.watch<FirebaseCloudFirestoreService>().streamInventoryType(_id);
+    return StreamBuilder<InventoryType>(
+      initialData: InventoryType(),
+      stream: _inventoryTypeStream,
+      builder: (context, snapshot) {
+        if (snapshot.hasError) {
+          return Scaffold(
+            body: Center(
+              child: Column(
+                children: [
+                  Text("Oops! Something went wrong"),
+                  IconButton(
+                    icon: Icon(Icons.arrow_back_outlined),
+                    onPressed: () => Navigator.of(context).pop(),
+                  ),
+                ],
+              ),
+            ),
+          );
+        }
+        if (snapshot.connectionState == ConnectionState.waiting) {
+          return Scaffold(
+            body: Center(
+              child: CircularProgressIndicator(),
+            ),
+          );
+        }
+
+        InventoryType _inventoryType = snapshot.data;
+
+        return Scaffold(
+          appBar: AppBar(
+            title: Text("${_inventoryType.name}"),
+          ),
+          body: ListView(
+            padding: EdgeInsets.all(16.0),
+            shrinkWrap: true,
+            children: [
+              Text("${_inventoryType.description}"),
+            ],
+          ),
+        );
+      },
+    );
+  }
+}
