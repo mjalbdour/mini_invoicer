3fb1390122c443e35a653a827037cdec5d6aeea0
MJALBDOUR <mjalbdour@gmail.com>
2021-03-13 03:46:16 +0200

    ++ProductAddScreen.ROUTE

--- mini_invoicer_client/lib/ui/routes/routes.dart
+++ mini_invoicer_client/lib/ui/routes/routes.dart
@@ -4,6 +4,7 @@ import 'package:flutter/widgets.dart';
 import 'package:mini_invoicer_client/ui/screens/auth/auth_wrapper.dart';
 import 'package:mini_invoicer_client/ui/screens/auth/auth_form_screen.dart';
 import 'package:mini_invoicer_client/ui/screens/home_screen.dart';
+import 'package:mini_invoicer_client/ui/screens/product/product_add_screen.dart';
 import 'package:mini_invoicer_client/ui/screens/product/products_screen.dart';
 
 Map<String, Widget Function(BuildContext)> routes = {
@@ -11,4 +12,5 @@ Map<String, Widget Function(BuildContext)> routes = {
   AuthFormScreen.ROUTE: (context) => AuthFormScreen(),
   HomeScreen.ROUTE: (context) => HomeScreen(),
   ProductsScreen.ROUTE: (context) => ProductsScreen(),
+  ProductAddScreen.ROUTE: (context) => ProductAddScreen(),
 };
