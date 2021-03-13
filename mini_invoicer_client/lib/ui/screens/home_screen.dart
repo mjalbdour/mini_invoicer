211816d124f0c2c6c99b5b9359e831f4d799d043
MJALBDOUR <mjalbdour@gmail.com>
2021-03-13 03:23:07 +0200

    ++Temp: Products Screen Button

--- mini_invoicer_client/lib/ui/screens/home_screen.dart
+++ mini_invoicer_client/lib/ui/screens/home_screen.dart
@@ -1,13 +1,17 @@
 import 'package:flutter/material.dart';
 import 'package:mini_invoicer_client/infrastructure/services/auth/firebase_auth_service.dart';
+import 'package:mini_invoicer_client/ui/screens/product/products_screen.dart';
 import 'package:provider/provider.dart';
 
 class HomeScreen extends StatelessWidget {
   static const String ROUTE = "/home";
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
+        title: Text("Home"),
+        centerTitle: true,
         actions: [
           IconButton(
             icon: Icon(Icons.exit_to_app_outlined),
@@ -17,7 +21,11 @@ class HomeScreen extends StatelessWidget {
         ],
       ),
       body: Center(
-        child: Text("home screen"),
+        child: ElevatedButton(
+          child: Text("Products"),
+          onPressed: () =>
+              Navigator.of(context).pushNamed(ProductsScreen.ROUTE),
+        ),
       ),
     );
   }
