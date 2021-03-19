8de1b4f6ecbf04c673fda7c644e5fe9c0c68881e
MJALBDOUR <mjalbdour@gmail.com>
2021-03-19 19:43:25 +0200

    ++edit action on appbar

--- mini_invoicer_client/lib/ui/screens/brand/brand_screen.dart
+++ mini_invoicer_client/lib/ui/screens/brand/brand_screen.dart
@@ -1,12 +1,13 @@
 import 'package:flutter/material.dart';
 import 'package:mini_invoicer_client/core/models/brand_model.dart';
 import 'package:mini_invoicer_client/infrastructure/services/db/firebase_cloud_firestore_service.dart';
+import 'package:mini_invoicer_client/ui/screens/brand/brand_update_screen.dart';
 import 'package:provider/provider.dart';
 
 class BrandScreen extends StatelessWidget {
   final String _id;
   BrandScreen(this._id);
-  
+
   @override
   Widget build(BuildContext context) {
     Stream<Brand> _brandStream =
@@ -43,6 +44,16 @@ class BrandScreen extends StatelessWidget {
         return Scaffold(
           appBar: AppBar(
             title: Text("${_brand.name}"),
+            actions: [
+              IconButton(
+                icon: Icon(Icons.edit),
+                onPressed: () => Navigator.of(context).push(
+                  MaterialPageRoute(
+                    builder: (context) => BrandUpdateScreen(_id),
+                  ),
+                ),
+              )
+            ],
           ),
           body: ListView(
             padding: EdgeInsets.all(16.0),
