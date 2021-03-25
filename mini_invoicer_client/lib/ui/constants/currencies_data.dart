924341420ed75c5a231e35d9ca796ffc9c5ca3fe
mjalbdour <mjalbdour@gmail.com>
2021-03-25 17:12:15 +0200

    +Currency list

---
+++ mini_invoicer_client/lib/ui/constants/currencies_data.dart
@@ -0,0 +1,19 @@
+import 'package:mini_invoicer_client/core/models/currency_model.dart';
+
+List<Currency> currencies = [
+  Currency(
+    name: "US Dollar",
+    code: "USD",
+    symbol: "\$",
+  ),
+  Currency(
+    name: "Euro",
+    code: "EUR",
+    symbol: "€",
+  ),
+  Currency(
+    name: "Jordanian Dinar",
+    code: "JOD",
+    symbol: "JD",
+  ),
+];
