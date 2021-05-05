5f4c12f2aacc0542e5b6c5f7ecdc39a6672b4a2a
mjalbdour <mjalbdour@gmail.com>
2021-05-05 17:18:46 +0300

    Deleted method: toJsonWithId. Rename attribute: name->title. Reformat

--- mini_invoicer_client/lib/core/models/product_transfer_type_model.dart
+++ mini_invoicer_client/lib/core/models/product_transfer_type_model.dart
@@ -1,29 +1,23 @@
 class ProductTransferType {
   String id;
-  String name;
+  String title;
   String description;
 
   ProductTransferType({
     this.id,
-    this.name,
+    this.title,
     this.description,
   });
 
   factory ProductTransferType.fromJson(String id, Map<String, dynamic> json) =>
       ProductTransferType(
         id: id,
-        name: json['name'] as String,
+        title: json['title'] as String,
         description: json['description'] as String,
       );
 
   Map<String, dynamic> toJson() => {
-        'name': this.name,
-        'description': this.description,
-      };
-
-  Map<String, dynamic> toJsonWithId() => {
-        'id': this.id,
-        'name': this.name,
+        'title': this.title,
         'description': this.description,
       };
 }
