e0582d04e6f0466ae648df4e16a3b1de45ca4d41
mjalbdour <mjalbdour@gmail.com>
2021-05-05 17:10:17 +0300

    Deleted method: toJsonWithId. Rename attribute: pricingId->pricingTypeId, value->price. Reformat

--- mini_invoicer_client/lib/core/models/product_pricing_model.dart
+++ mini_invoicer_client/lib/core/models/product_pricing_model.dart
@@ -1,49 +1,39 @@
 class ProductPricing {
   String id;
-  double value;
+  double price;
   int salesTax;
   String description;
   String productId;
-  String pricingId;
+  String pricingTypeId;
   String currencyId;
 
   ProductPricing({
     this.id,
-    this.value,
+    this.price,
     this.salesTax,
     this.description,
     this.productId,
-    this.pricingId,
+    this.pricingTypeId,
     this.currencyId,
   });
 
   factory ProductPricing.fromJson(String id, Map<String, dynamic> json) =>
       ProductPricing(
         id: id,
-        value: json['value'] as double,
+        price: json['price'] as double,
         salesTax: json['salesTax'] as int,
         description: json['description'] as String,
         productId: json['productId'] as String,
-        pricingId: json['pricingId'] as String,
+        pricingTypeId: json['pricingTypeId'] as String,
         currencyId: json['currencyId'] as String,
       );
 
   Map<String, dynamic> toJson() => {
-        'value': this.value,
+        'price': this.price,
         'salesTax': this.salesTax,
         'description': this.description,
         'productId': this.productId,
-        'pricingId': this.pricingId,
-        'currencyId': this.currencyId,
-      };
-
-  Map<String, dynamic> toJsonWithId() => {
-        'id': this.id,
-        'value': this.value,
-        'salesTax': this.salesTax,
-        'description': this.description,
-        'productId': this.productId,
-        'pricingId': this.pricingId,
+        'pricingTypeId': this.pricingTypeId,
         'currencyId': this.currencyId,
       };
 }
